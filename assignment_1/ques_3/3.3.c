da2f9f796307c46a5302e253f55a16a50baca798
Himanshu Kumar Gupta <ai21btech11012@iith.ac.in>
2022-08-07 22:12:37 +0530

    commiting 3.3

--- assignment_1/ques_3/3.3.c
+++ assignment_1/ques_3/3.3.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
 
+double y(int n){
 
+    if(n<0){
+        return 0;
+    }
+    if(n>=0){
+        return (double)-1/2*y(n-1)+x(n)+x(n-2);
+    }
+}
 
+int x(int n){
+    int x_arr[]={1,2,3,4,2,1};
+    if(n<0||n>=6){
+        return 0;
+    }
+    else{
+        return x_arr[n];
+    }
+}
 
 int main(){
-    int x[]={1,2,3,4,2,1};
 
-    float y[10];
-    y[0]=x[0]
-    y[1]=-1/2*y[0]+x[1]
-
-    
-
-
-printf("%d",x[1]);
-
-    // y[0]=/
+    // float y[10];
+    // y[0]=x[0]
+    // y[1]=-1/2*y[0]+x[1]
 
 
 
+for(int i=-5;i<10;i++){
+printf("%f ",y(i));
+
+}
     return 0;
 }
--- assignment_1/ques_5/5.5.py
+++ assignment_1/ques_5/5.5.py
@@ -9,22 +9,33 @@ def delta(n):
     else:
         return 0
         
-len=10
-h=np.zeros(len)
+leng=10
+h=np.zeros(leng)
 h[0]=1
 
-for i in range(1,len):
+for i in range(1,leng):
     h[i]=-h[i-1]/2+delta(i)+delta(i-2)
 
-x=[1,2,3,4,2,1]
-print(len(x))
-tople=np.zeros([len(h)+len(x)-1,len(x)])
-print(tople)
-n=np.linspace(0,len,len)
 
+x=[1,2,3,4,2,1]
+
+
+tople=np.zeros([len(h)+len(x)-1,len(x)])
+
+n=np.linspace(0,len(h)+len(x)-2,len(h)+len(x)-1)
+print(n)
+for i in range(len(x)):
+    for j in range(len(h)):
+        tople[j+i][i]=h[j]
 
 # y=np.convolve(h[0:10],x)
-# print(y)
 
+y=np.dot(tople,x)
+
+plt.stem(n,y)
+plt.ylabel("$y(n)$")
+plt.xlabel("$n$")
+plt.grid()
+plt.show()
 
 
